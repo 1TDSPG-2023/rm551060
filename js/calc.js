3263f087ea7c316354665ff6b7e4604beac9da21
Nick-Rodrigues <RM551060@fiap.com.br>
2023-05-12 12:13:50 -0300

    Calculadora básica finalizada.

--- js/calc.js
+++ js/calc.js
@@ -1,5 +1,13 @@
 function insereNumeroNoVisor(botao,visor) {
-    visor.value += botao.value;
+    if(botao.value === "+" || botao.value === "-" || botao.value === "*" || botao.value === "/"){
+        if(visor.value[visor.value.length - 1] == "+" || visor.value[visor.value.length - 1] == "-" || visor.value[visor.value.length - 1] == "*" || visor.value[visor.value.length - 1] == "/"){
+            visor.value.slice(visor.value.length - 1);
+        } else {
+            visor.value += botao.value
+        }
+    } else {
+        visor.value += botao.value;
+    }
 }
 
 const resultadoDasOperacoes = (visor) =>{
